ecc209280a0d71e95c842167ca8973776fd6db29
Orlane <orlanedoumbe@gmail.com>
2022-02-23 11:59:08 +0000

    Made the FizzBuzz code more clear by creating different sections

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,29 @@
 
-# Divisible / 3 = fizz
-# divisble / 5 = buzz
-# both = fizzbuzz
-# else = number
+# FizzBuzz game
+
+# Criteria:
+# Get user to input an integer number
+# If number is divisible by 3 and 5, output = Fizzbuzz
+# If number is divisible by 3, output = fizz
+# If number is divisible by 5, output = buzz
+# Else, output = number
 
 
+puts "Type an integer number: "
+number = gets.chomp.to_i
 
-# if number.modulo(3) == 0 && number.modulo(5)
-#     return "Fizzbuzz"
-# elsif number.modulo(3)
-#     return "Fizz"
-# elsif number.modulo(5) 
-#     return "Buzz"
-# else 
-#     return number
-# end
+if(number % 3 == 0) && (number % 5 == 0)
+    puts "FizzBuzz"
+elsif (number % 3 == 0)
+    puts "Fizz"
+elsif (number % 5 == 0)
+    puts "Buzz"
+else 
+    puts number
+end
 
 
-
+# Previous attempt:
 
 # if (number % 3 == 0) && (number % 5 == 0)
 #     return "Fizzbuzz"
@@ -28,16 +34,3 @@
 # else 
 #     return number
 # end
-
-puts "Type an integer number: "
-number = gets.chomp.to_i
-
-if number % 3 == 0 && number % 5 == 0
-    puts "Fizzbuzz"
-elsif number % 3 == 0
-    puts "Fizz"
-elsif number % 5 == 0
-    puts "Buzz"
-else 
-    puts number
-end
